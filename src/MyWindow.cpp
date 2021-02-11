69a1969ae8259f0c80ab6ec8ed11d81a4716abf8
Nikita <42209767+impassive-nik@users.noreply.github.com>
2021-02-12 00:01:11 +0300

    Use human textures to emulate simple gameplay

--- src/MyWindow.cpp
+++ src/MyWindow.cpp
@@ -7,19 +7,26 @@ MyWindow::MyWindow(ResManager* mgr) : NativeWindow("MyGame"), res_mgr(mgr) {
     res_mgr = ResManager::getInstance();
 }
 
+enum class CellTy : int {
+  EMPTY,
+  WALL,
+  ENEMY
+};
+
 void MyWindow::run() {
   IM_ASSERT(ImGui::GetCurrentContext() != nullptr && "Missing dear imgui context. Refer to examples app!");
 
   static bool show_demo_window = false;
   static bool first_iteration = true;
-  static Texture *Cell_tex[2];
-  static Texture *Body_tex;
+  static Texture *Cell_tex[3];
+  static Texture *Buddy_tex;
 
   if (first_iteration) {
     first_iteration = false;
     Cell_tex[0] = res_mgr->get(TexturesKind::Cell_1);
     Cell_tex[1] = res_mgr->get(TexturesKind::Cell_2);
-    Body_tex    = res_mgr->tryLoad("Body", "Body.png");
+    Cell_tex[2] = res_mgr->get(TexturesKind::Enemy);
+    Buddy_tex   = res_mgr->get(TexturesKind::Buddy);
   }
   
   // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
@@ -35,10 +42,10 @@ void MyWindow::run() {
 
     ImGui::Begin("MainWindow", nullptr, window_flags);
 
-    static Grid<bool> my_grid = {5, 5, false};
+    static Grid<CellTy> my_grid = {5, 5, CellTy::EMPTY};
     static int my_x = 0, my_y = 0;
 
-    if (Cell_tex[0] && !my_grid.empty()) {
+    if (!my_grid.empty()) {
       int Cell_ID = 0;
       for (int y = 0; y < my_grid.getHeight(); ++y) {
         for (int x = 0; x < my_grid.getWidth(); ++x) {
@@ -48,21 +55,49 @@ void MyWindow::run() {
           auto last_x = ImGui::GetCursorPosX();
 
           ImGui::PushID(Cell_ID++);
-          bool &val = my_grid.at(x, y);
-          if (ImGui::ImageButton(Cell_tex[val]->ID, Cell_tex[val]->size)) {
-            if (Body_tex && std::abs(my_x - x) + std::abs(my_y - y) == 1) {
-              my_x = x;
-              my_y = y;
+          CellTy &val = my_grid.at(x, y);
+          if (ImGui::ImageButton(Cell_tex[(int) val]->ID, Cell_tex[(int) val]->size)) {
+            int dx = std::abs(my_x - x) + std::abs(my_y - y);
+            if (dx == 1) {
+              switch (val) {
+              case CellTy::EMPTY:
+                my_x = x;
+                my_y = y;
+                break;
+              case CellTy::WALL:
+                break;
+              case CellTy::ENEMY:
+                val = CellTy::EMPTY;
+                break;
+              }
+            } else if (dx == 0) {
+              if (my_x > 0 || my_y > 0) {
+                val = CellTy::ENEMY;
+                my_x = 0;
+                my_y = 0;
+              }
             } else {
-              val = !val;
+              switch (val) {
+              case CellTy::EMPTY:
+                val = CellTy::WALL;
+                break;
+              case CellTy::WALL:
+                val = CellTy::EMPTY;
+                break;
+              default:
+                break;
+              }
             }
           }
           ImGui::PopID();
 
-          if (Body_tex && x == my_x && y == my_y) {
+          if (x == my_x && y == my_y) {
             auto cur_x = ImGui::GetCursorPosX();
             ImGui::SameLine(last_x);
-            ImGui::ImageButton(Body_tex->ID, Body_tex->size);
+            if (ImGui::ImageButton(Buddy_tex->ID, Buddy_tex->size)) {
+              my_x = 0;
+              my_y = 0;
+            }
             ImGui::SetCursorPosX(cur_x);
           }
         }
